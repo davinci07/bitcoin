b863f0acb5782917a79f7e1d75fdc249a0f4986f
Nikita Lysov <lysov.nikita@gmail.com>
2019-07-11 18:18:41 -0500

    Add files via upload

--- bitcoin.py
+++ bitcoin.py
@@ -1,49 +1,43 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jun 27 19:41:10 2019
-
-@author: Asus
-"""
 
 import requests as req
 import pandas as pd
 import json
 import csv
+import matplotlib.pyplot as plt
+
 
 # Set pu URL for the program to talk to
+def crypto(ticker):
+    base = 'https://api.pro.coinbase.com'   # the url base
+    qqq = '/products/%s/candles' % (ticker)  # the page  #candles used to be trades -- see coinbase website
+    url = base + qqq
+    #start = "2017-01-07T23:00:00.000Z"
+    #end = "2017-01-08T04:00:00.000Z"
 
-base = 'https://api.pro.coinbase.com'   # the url base
-qqq = '/products/BTC-USD/candles'  # the page  #candles used to be trades -- see coinbase website
-url = base + qqq
+    times = [["2017-01-07T23:00:00.000Z","2017-01-08T04:00:00.000Z"],
+    ["2017-01-08T04:00:00.000Z", "2017-01-08T09:00:00.000Z"]]
 
+    df = pd.DataFrame()
 
-ticks = []
-start = "2017-01-07T23:00:00.000Z"
-end = "2017-01-07T23:20:00.000Z"
+    for i in times:
+        r = req.get(url=url, params = {'start' : i[0], 'end' : i[1], 'granularity' : 60})
+        df = df.append(r.json())
 
-r = req.get(url=url, params = {'start' : start, 'end' : end, 'granularity' : 60})
-ticks.append(r.json())
+    df.columns=(["time", "low", "high", "open", "close", "volume"])
+    df = df.sort_values("time")
+    df["time"] = pd.to_datetime(df['time'],unit='s')
+    return(df)
 
-df = pd.DataFrame.from_dict(ticks)
-df.to_csv('csv_bitc.csv', index=False)
+    #df.plot(kind='line', x="time", y='open', color="green")
+    #plt.show()
+    #df = pd.DataFrame(r.json())
+    #df.columns=(["time", "low", "high", "open", "close", "volume"])
+    #df.to_csv('%s.csv' % (ticker), index=False)
 
-#with open('bit_csv.csv', 'w',newline='') as fp:
- #   a = csv.writer(fp, delimeter=',')
-  #  a.writerows(df)
+btc_usd = crypto("btc-usd")
+ltc_usd = crypto("ltc-usd")
+#eth_usd = crypto("eth-usd")
 
-#for i in range(5):
- #   r = req.get(url=url)
- #   ticks.append(r.json())
-
-#time is unix
-
-#print(pd.DataFrame(ticks))    
-    
-
-#print(r.text)
-#print(pd.DataFrame.from_dict([r.json()]))
-
-
-#then go to terminal  #cd.. gets rid of and cd Desktop adds
-#python bitcoin.py   in terminal
-#b'{  is a json object
+btc_usd["spread"] = btc_usd["open"] - btc_usd["close"]
+btc_usd["spread"] = btc_usd["spread"].apply((lambda x: "Negative" if x < 0 else x))
+btc_usd.to_csv('%s.csv' % ("btc-usd"), index=False)
